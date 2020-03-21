9abc08c9b9642c156e6cb0e2b119681778905d67
MaseDar <mailspecialforspam@mail.ru>
2020-03-21 15:52:48 +0300

    делаем задание

---
+++ practice-two/auto.rb
@@ -0,0 +1,11 @@
+class Auto
+    def initilize(brand,model,manufacture_year,gasoline_consumption)
+        @brand = brand
+        @model = model
+        @manufacture_year = manufacture_year
+        @gasoline_consumption = gasoline_consumption
+    end
+    def to_s
+        
+    end
+end
