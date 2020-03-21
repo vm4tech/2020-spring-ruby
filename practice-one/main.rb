d2bc3460e8ad907e4914a6b37483b9c2b1534fc0
MaseDar <mailspecialforspam@mail.ru>
2020-03-21 15:25:02 +0300

    добавил создание 3 объекта из суммы двух других

--- practice-one/main.rb
+++ practice-one/main.rb
@@ -3,5 +3,9 @@ require_relative 'my_complex.rb'
 first = MyComplex.new(3,2)
 second = MyComplex.new(2,3)
 
-pp first
-pp second
+puts first.to_s
+puts second
+
+pp first.real
+thirty = first.add(second)
+puts thirty.to_s
--- practice-one/my_complex.rb
+++ practice-one/my_complex.rb
@@ -1,9 +1,31 @@
 class MyComplex
-   def initialize(real,imaginary)
+    def initialize(real,imaginary)
         @real = real
         @imaginary = imaginary   
-   end
-   def sum
-   end
+    end
+    
+    def add (other) #метод создания объекта из сумм 2-з других объектов
+        new_real = other.real + @real
+        new_imaginary = other.imaginary + @imaginary
+        return MyComplex.new(new_real,new_imaginary)
+    end
+
+    def to_s
+        "#{@real} + i#{@imaginary}"
+    end
+    def real #атрибут чтения real
+        @real
+    end
+    def imaginary #атрибут чтения imiginary
+        @imaginary
+    end
+    
+    def other
+        @
+    end
+
+    def multi
+    end
+
 end
 
