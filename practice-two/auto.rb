a838cc24c353b2868cd5fef5e9092a9c9559b5d4
MaseDar <mailspecialforspam@mail.ru>
2020-04-02 14:05:39 +0300

    add gem's and rubocop

--- practice-two/auto.rb
+++ practice-two/auto.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
+# doc
 class Auto
-    def initialize(brand,model,manufacture_year,gasoline_consumption)
-        @brand = brand
-        @model = model
-        @manufacture_year = manufacture_year
-        @gasoline_consumption = gasoline_consumption
-    end
-    def to_s
-        "\t Марка: #{@brand}
+  def initialize(brand, model, manufacture_year, gasoline_consumption)
+    @brand = brand
+    @model = model
+    @manufacture_year = manufacture_year
+    @gasoline_consumption = gasoline_consumption
+  end
+
+  def to_s
+    "\t Марка: #{@brand}
          Модель: #{@model}
          Год выпуска: #{@manufacture_year}
          Расход бензина на 100км: #{@gasoline_consumption}"
-    end
+  end
 end
-
--- practice-two/main.rb
+++ practice-two/main.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require_relative 'auto.rb'
 
-Nissan_Passat = Auto.new("Nissan","Passat",2020,8)
-Lada_Priora = Auto.new("Lada","Priora",1,5)
-Mersedes_Benz_AMG_GT = Auto.new("Mersedes Benz","AMG GT",2018,16)
+Nissan_Passat = Auto.new('Nissan', 'Passat', 2020, 8)
+Lada_Priora = Auto.new('Lada', 'Priora', 1, 5)
+Mersedes_Benz_AMG_GT = Auto.new('Mersedes Benz', 'AMG GT', 2018, 16)
 
 puts Nissan_Passat.to_s
 puts Lada_Priora.to_s
